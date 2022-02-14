5493df2fd8460ab6861eb63b713a33e4685da1e4
damonspencerroberts <damonspencerroberts@gmail.com>
2022-02-14 15:21:32 +0000

    handle state in the form

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import Button from "./components/button";
 import Form from "./components/form";
 import Header from "./components/header";
 import MainContainer from "./containers/mainContainer";
+import NoteContext from "./context/NoteContext";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [notes, setNotes] = useState([]);
   return (
-    <MainContainer>
-      <Header>Chillipharm Note Pad</Header>
-      {showForm ? (
-        <Form onExitClick={() => setShowForm(false)} />
-      ) : (
-        <Button
-          style={{ background: "#6EE5F5" }}
-          onClick={() => setShowForm(true)}
-        >
-          Add a note
-        </Button>
-      )}
-    </MainContainer>
+    <NoteContext.Provider value={{ notes, setNotes }}>
+      <MainContainer>
+        <Header>Chillipharm Note Pad</Header>
+        {showForm ? (
+          <Form handleExitClick={() => setShowForm(false)} />
+        ) : (
+          <Button
+            style={{ background: "#6EE5F5" }}
+            onClick={() => setShowForm(true)}
+          >
+            Add a note
+          </Button>
+        )}
+      </MainContainer>
+    </NoteContext.Provider>
   );
 }
 
--- src/components/form.js
+++ src/components/form.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import styled from "styled-components";
+import NoteContext from "../context/NoteContext";
 import Button from "./button";
 import Input from "./input";
 import TextBox from "./textbox";
 
 const Form = (props) => {
-  const { onExitClick = () => {} } = props;
+  const tester = useContext(NoteContext);
+  const [formData, setFormData] = useState({
+    name: "",
+    date: "",
+    note: "",
+  });
+
+  const handleFormChange = (event, key) => {
+    const dupFormData = { ...formData };
+    dupFormData[key] = event.target.value;
+    setFormData(dupFormData);
+  };
+  
   return (
     <StyledForm {...props}>
       <InnerForm>
-        <StyledExit onClick={onExitClick}>X</StyledExit>
+        <StyledExit onClick={props.handleExitClick}>X</StyledExit>
         <Input
           type="text"
           placeholder="Your Name"
           style={{ marginBottom: 10 }}
+          onChange={(e) => handleFormChange(e, "name")}
+          value={formData.name}
         />
-        <Input type="date" style={{ marginBottom: 10 }} />
-        <TextBox placeholder="Your Note" style={{ marginBottom: 10 }} />
+        <Input
+          type="date"
+          style={{ marginBottom: 10 }}
+          value={formData.date}
+          onChange={(e) => handleFormChange(e, "date")}
+        />
+        <TextBox
+          placeholder="Your Note"
+          style={{ marginBottom: 10 }}
+          onChange={(e) => handleFormChange(e, "note")}
+          value={formData.note}
+        />
         <Button type="submit">Submit</Button>
       </InnerForm>
     </StyledForm>
