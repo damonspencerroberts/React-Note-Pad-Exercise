534a29e7bd1a69ca6fc7f02ef79c4a8a5172e305
damonspencerroberts <damonspencerroberts@gmail.com>
2022-02-14 15:40:29 +0000

    add notes

--- src/components/notes.js
+++ src/components/notes.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import Note from "./note";
 import styled from "styled-components";
+import NoteContext from "../context/NoteContext";
 
 const Notes = () => {
+  const { notes } = useContext(NoteContext);
+  const notesLength = notes.length;
   return (
-    <Note
-      note={{ name: "Damon", date: "02-02-2022", note: "this is a note" }}
-    />
+    <>
+      {notesLength > 0 ? (
+        <>
+          {notes.map((note, index) => {
+            return <Note key={index} note={note} />;
+          })}
+        </>
+      ) : (
+        <StyledEmptyNotes>Current no notes</StyledEmptyNotes>
+      )}
+    </>
   );
 };
 
 export default Notes;
+
+const StyledEmptyNotes = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 32px;
+  font-weight: 400;
+  color: red;
+`;
